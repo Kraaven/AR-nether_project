0f343e037363faa1a6f027924f5e7cba9cfaaa2e
Kraaven <dhruv.bhojaraj@gmail.com>
2023-09-21 23:16:27 +0530

    Initial commit

---
+++ Assets/script/ARscript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARscript : MonoBehaviour
+{
+
+    public ARPlaneManager planemanager;
+    public ARRaycastManager raycastmanager;
+
+    public GameObject Netherportal;
+
+    private bool placed = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        planemanager = GameObject.Find("AR Session Origin").GetComponent<ARPlaneManager>();
+        raycastmanager = GameObject.Find("AR Session Origin").GetComponent<ARRaycastManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            List<ARRaycastHit> hits = new List<ARRaycastHit>();
+            bool hit_true = raycastmanager.Raycast(Input.GetTouch(0).position, hits, TrackableType.PlaneWithinPolygon);
+
+            if (hit_true && placed == false)
+            {
+                ARRaycastHit hit = hits[0];
+                ARPlane Touch_Plane = planemanager.GetPlane(hit.trackableId);
+            }
+        }
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AR-nether_project
+ Using the knowledge learnt in my AR Unity course, i am trying to create a Unity AR project that creates a nether portal from Minecraft
